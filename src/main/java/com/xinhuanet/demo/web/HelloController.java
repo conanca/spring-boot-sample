509780220b0d73f260567e31ddd49db04ab115b1
conanca <imconan@gmail.com>
2016-06-17 14:31:55 +0800

    增加鉴权示例

--- src/main/java/com/xinhuanet/demo/web/HelloController.java
+++ src/main/java/com/xinhuanet/demo/web/HelloController.java
@@ -2,6 +2,7 @@ package com.xinhuanet.demo.web;
 
 import com.xinhuanet.demo.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
--- src/main/java/com/xinhuanet/demo/web/config/WebSecurityConfig.java
+++ src/main/java/com/xinhuanet/demo/web/config/WebSecurityConfig.java
@@ -3,12 +3,14 @@ package com.xinhuanet.demo.web.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -30,7 +32,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("password").roles("ADMIN");
     }
 
 }
