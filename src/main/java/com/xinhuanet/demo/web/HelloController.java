334a880f9a7b1aa99cc7ce3fea1f3a9b88e336f5
conanca <imconan@gmail.com>
2016-06-17 13:57:55 +0800

    增加spring security 和 thymeleaf 示例

--- src/main/java/com/xinhuanet/demo/web/HelloController.java
+++ src/main/java/com/xinhuanet/demo/web/HelloController.java
@@ -2,18 +2,35 @@ package com.xinhuanet.demo.web;
 
 import com.xinhuanet.demo.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HelloController {
 
     @Autowired
     private HelloService helloService;
 
-    @RequestMapping("/")
+    @RequestMapping("/sayHello")
+    @ResponseBody
     public String home() {
         helloService.say();
         return "Hello World!";
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
     }
 }
