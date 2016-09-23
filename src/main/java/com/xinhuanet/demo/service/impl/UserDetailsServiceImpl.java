f4f3710b7250559997b34067a84605b5e50c10e4
conanca <imconan@gmail.com>
2016-09-23 10:57:00 +0800

    调整注释

--- src/main/java/com/xinhuanet/demo/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/xinhuanet/demo/service/impl/UserDetailsServiceImpl.java
@@ -10,8 +10,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -26,8 +24,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
-
-    static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
